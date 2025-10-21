8e927a6f094d4f74d934ed803a5874d8ad9b2eec
Franco Mariño <123432025+fmarinoa@users.noreply.github.com>
2025-10-20 23:42:17 -0500

    Add CI workflow (#2)
    * chore(husky): add pre-push linting and formatting checks
    
    * feat(user): add create user feature scenario
    
    * fix(cucumber): correct feature path pattern
    
    * feat(cucumber): add HTML report format option
    
    * feat(tests): add scenario for user creation failure
    
    * chore(gitignore): add cucumber report files to ignore
    
    * feat(ci): add GitHub Actions workflow for API tests
    
    * chore(ci): remove cache option for pnpm setup
    
    * fix(pnpm): update typescript-eslint specifier version
    
    * fix(eslint): update ignores and add parser options
    
    * style(workflow): format YAML and improve readability
    
    * style(eslint): format ignores for improved readability
    
    * docs(readme): update project description for clarity
    
    * fix(json): correct user data structure in request
    
    * ci(workflow): improve YAML formatting for readability
    
    * fix(ci): remove working directory
    
    * fix(ci): create folder to report
    
    * ci(workflow): add id-token permission for pages
    
    * fix(ci): restrict branches to 'main' for workflow

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,22 @@
 export default [
   {
-    ignores: ['dist/**/*', 'node_modules/**/*'],
+    ignores: [
+      'dist/**/*',
+      'node_modules/**/*',
+      'commitlint.config.ts',
+      'eslint.config.ts',
+    ],
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {},
   },
 ]
